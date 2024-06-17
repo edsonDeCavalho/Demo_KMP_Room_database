390b075bd58afe87dfb5ad8a6829f1fb03a64adf
Edson De Carvalho <edson_carvalho@gmx.fr>
2024-06-17 11:34:38 +0200

    kmp_database_without_ios

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -38,16 +38,16 @@ kotlin {
     
     jvm("desktop")
     
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-    baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
+  //  listOf(
+  //      iosX64(),
+  //      iosArm64(),
+  //      iosSimulatorArm64()
+  //  ).forEach { iosTarget ->
+  //      iosTarget.binaries.framework {
+  //  baseName = "ComposeApp"
+  //          isStatic = true
+  //      }
+  //  }
     sourceSets.all {
         languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
     }
